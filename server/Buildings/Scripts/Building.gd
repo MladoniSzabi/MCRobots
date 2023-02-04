76040a399da87b995b70d99e173c6b0546a01aea
MladoniSzabi <szabimlado@gmail.com>
2023-02-04 14:05:29 +0000

    added base building class. populated it with random blocks for testing.

---
+++ server/Buildings/Scripts/Building.gd
@@ -0,0 +1,11 @@
+extends GridMap
+
+export var padding_left = 0
+export var padding_right = 0
+export var padding_front = 0
+export var padding_back = 0
+export var padding_top = 0
+export var padding_bottom = 0
+
+func _ready():
+	pass
