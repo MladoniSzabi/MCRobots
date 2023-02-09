19e0ec104d6d284e75feffcb706c312cac4afb8d
MladoniSzabi <szabimlado@gmail.com>
2023-02-09 14:21:15 +0000

    added code to zoom the camera in and out.

--- server/Scripts/CameraController.gd
+++ server/Scripts/CameraController.gd
@@ -1,6 +1,8 @@
 extends Camera
 
 var previous_robot_transform = [Vector3(), Vector3()]
+const ZOOM_STEP = 0.8
+const ZOOM_STEP_FINE = 0.1
 
 func _ready():
 	pass
@@ -15,17 +17,15 @@ func follow_robot(robot, world: GridMap):
 	translation = world.to_global(robot.position + robot.orientation*0.1 + Vector3(0.5,0.6,0.5))
 	look_at(world.to_global(robot.position + Vector3(0.5,0.5,0.5) + robot.orientation), Vector3.UP)
 
+
 func _input(event):
-	# TODO: Use the input map instead
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_WHEEL_UP:
-			fov += 1
-		elif event.button_index == BUTTON_WHEEL_DOWN:
-			fov -= 1
-	
-	# TODO: use this instead
-	#var step = 2
-	#var camera_position = camera.global_transform.origin
-	#var target_position = target.get_global_transform().origin
-	#var zoom = camera_position.move_toward(target_position, step)
-	#camera.global_transform.origin = zoom
+	if event.is_action_pressed("camera_zoom_in"):
+		var step = ZOOM_STEP_FINE if Input.is_action_pressed("finer_control") else ZOOM_STEP
+		var forward = global_transform.basis.z
+		var zoom = global_transform.origin.move_toward(global_translation - forward, step)
+		global_transform.origin = zoom
+	elif event.is_action_pressed("camera_zoom_out"):
+		var step = ZOOM_STEP_FINE if Input.is_action_pressed("finer_control") else ZOOM_STEP
+		var forward = global_transform.basis.z
+		var zoom = global_transform.origin.move_toward(global_translation + forward, step)
+		global_transform.origin = zoom
